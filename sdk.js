0bb3068da6a2e7e8dabbcd9fed47cfe886e3f4a2
mjadsmurai <142219663+mjadsmurai@users.noreply.github.com>
2023-08-16 15:10:21 +0200

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -1,21 +1,5 @@
 class AdsmuraiSDK {
-    async post (key, data, url) {
-        try {
-            const response = await fetch(url ? url : "https://ev.st.adsmurai.com/v1.0/bulk", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Auth-Token": key,
-                },
-                body: JSON.stringify(data),
-            });
-            return await response.json();
-        } catch (e) {
-            return null;
-        }
-    }
-
-    post2 (key, data, url) {
+    post (key, data, url) {
         let xhr = new XMLHttpRequest();
         xhr.open("POST", url ? url : "https://ev.st.adsmurai.com/v1.0/bulk");
         xhr.setRequestHeader("Content-type", "application/json");
