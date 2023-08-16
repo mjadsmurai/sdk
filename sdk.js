c4b4091864a83587f27b18d1a2fe0dcd463b3d8e
mjadsmurai <142219663+mjadsmurai@users.noreply.github.com>
2023-08-16 16:18:10 +0200

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -1,14 +1,21 @@
 class AdsmuraiSDK {
     post (key, data, url) {
-        let xhr = new XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
         xhr.open("POST", url ? url : "https://ev.st.adsmurai.com/v1.0/bulk");
         xhr.setRequestHeader("Content-type", "application/json");
         xhr.setRequestHeader("Auth-Token", key);
         xhr.send(JSON.stringify(data));
     }
 
-    fetch (resource, options) {
-        return window.fetch(resource, options);
+    fetch (url) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.withCredentials = true;
+            xhr.addEventListener("load", resolve);
+            xhr.addEventListener("error", reject);
+            xhr.send(null);    
+        });
     }
 
     pushEvent () {
